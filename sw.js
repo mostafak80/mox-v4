d20c068af581ae6d3215c077a1c6ca18cb421042
mostafak80 <aboab411@gmail.com>
2026-09-14 03:44:38 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE='mox-v2-clean-v1';
+const CORE=['./','./index.html','./style.css','./app.js','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>Promise.allSettled(CORE.map(u=>c.add(u))))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(fetch(e.request).then(r=>{if(r&&r.ok){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{})}return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+});
